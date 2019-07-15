acf5b75059943b6780746fd15d5b383912fd4eca
Cinthia Lima <cinty.calderon.15@gmail.com>
2019-07-15 15:59:59 -0300

    Se comenzo con la implementacion del CU20: ver formularios

--- src/pages/formularios/formularios.ts
+++ src/pages/formularios/formularios.ts
@@ -8,12 +8,12 @@ import { NavController } from 'ionic-angular';
 export class FormulariosPage {
   formularios = [
     {
-      titulo: 'Encuesta de nivel de atencion en el buffet',
+      titulo: 'Encuesta de nivel de atencion en el buffet fdf sdf sdfdsf dsf sd fds fd fds f',
       descripcion: 'Con este formulario se desea conocer el nivel de atención del personal que actualente trabaja e el buffet de la universidad.'
     },
     {
       titulo: 'Certificado de alumno regular',
-      descripcion: 'Formulario para solicitar el certificado de alumno regular.'
+      descripcion: 'Formulario para solicitar el certificado de alumno regular. fdfdfjd ddddd d ddd dddddddd dddd dddddddd ddddddd dddddddddd ddddddd ddddddddd ddddd ddddddddddd ddddddddddddd ddddddddddddddddd dddddddd dd dddddd ddddddd ddd ddddd dddddd dd fdf fdsf fd f'
     },
     {
       titulo: 'Formulario 4',
